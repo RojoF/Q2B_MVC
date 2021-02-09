efe83e1e27744d3752ef755ba35f3d889e85d4aa
RojoF <rojof83@gmail.com>
2021-02-09 12:22:57 +0100

    Subir proyecto

--- Controllers/ImagenesController.cs
+++ Controllers/ImagenesController.cs
@@ -81,6 +81,7 @@ namespace Q2B_MVC.Controllers
         public ActionResult Edit([Bind(Include = "Id,Titulo,Descripcion,UrlImagen")] Imagenes imagenes)
         {
 
+
             if (ModelState.IsValid)
             {
                 db.Entry(imagenes).State = EntityState.Modified;
